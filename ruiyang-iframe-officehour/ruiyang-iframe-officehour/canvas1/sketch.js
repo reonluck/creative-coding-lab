fdb94236289a359aaf96cbc54b5fb83666520b53
reonluck <99263352+reonluck@users.noreply.github.com>
2022-05-09 21:42:42 +0800

    5

--- ruiyang-iframe-officehour/ruiyang-iframe-officehour/canvas1/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent("canvasContainer")
-}
-
-function draw() {
-  background(220);
-  text(width, width/2, height/2)
-}
